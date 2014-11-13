24bc4547c5d41372f6cc06abf02fcdeb68455173
Will Faurot <wfaurot@gmail.com>
2014-11-12 20:19:22 -0700

    Refactor client.rb

--- lib/tunefish/client.rb
+++ lib/tunefish/client.rb
@@ -7,32 +7,43 @@ module Tunefish
   class Client
     attr_reader :connection
 
-    def initialize(host)
+    def initialize(host, version = "v1")
       @connection = Faraday.new(url: host)
+      @version = version
     end
 
     def user(id)
-      response = connection.get("/api/v1/users/#{id}")
-      data = JSON.parse(response.body)
+      response = connection.get("#{base_url}/users/#{id}")
+      data = parse_response_body(response.body)
       User.new(data["user"])
     end
 
     def users
-      response = connection.get("/api/v1/users")
-      data = JSON.parse(response.body)
+      response = connection.get("#{base_url}/users")
+      data = parse_response_body(response.body)
       data["users"].map { |user_data| User.new(user_data) }
     end
 
     def user_activities(id)
-      response = connection.get("/api/v1/users/#{id}/activities")
-      data = JSON.parse(response.body)
+      response = connection.get("#{base_url}/users/#{id}/activities")
+      data = parse_response_body(response.body)
       data["activities"].map { |activity_data| Activity.new(activity_data) }
     end
 
     def activities
-      response = connection.get("/api/v1/activities")
-      data = JSON.parse(response.body)
+      response = connection.get("#{base_url}/activities")
+      data = parse_response_body(response.body)
       data["activities"].map { |activity_data| Activity.new(activity_data) }
     end
+
+    private
+
+      def parse_response_body(body)
+        JSON.parse(body)
+      end
+
+      def base_url
+        "/api/#{@version}"
+      end
   end
 end
